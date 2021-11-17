e92679ec9f54a659e5e6ee73f21237dafe8d3384
Erwann Le Noac'h <erwann.lenoach@hotmail.fr>
2021-11-17 10:13:02 +0100

    show info

--- src/cover/index.js
+++ src/cover/index.js
@@ -1,63 +1,106 @@
 import React, {useState, useEffect} from 'react';
-import {View, Image, StyleSheet}  from 'react-native';
+import {View, Image, StyleSheet, Text, SafeAreaView, Button} from 'react-native';
 
 const Cover = () => {
- 
   const [url, setUrl] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [time, setTime] = useState(null);
 
   useEffect(() => {
     myFetch();
-    console.log(url)
-  }, [])
+  }, []);
 
   async function myFetch() {
     const cheerio = require('react-native-cheerio');
+
     let response = await fetch('https://hotelradioparis.com/');
     let text = await response.text();
     const $ = cheerio.load(text);
     let images = $.html('[class="proradio-slider__i wp-post-image"]');
     let src = $(images).attr('src');
-   setUrl(src);
+    let titleHTML = $.html(
+      '[class="proradio-customplayer__showtitle proradio-cutme-t"]',
+    );
+    let title = $(titleHTML).text().trim();
+    let timeHTML = $.html(
+      '[class="proradio-customplayer__time proradio-itemmetas"]',
+    );
+    
+    let time = $(timeHTML).text().trim().replace('access_time', '')
+    console.log(time)
+
+    setUrl(src);
+    setTitle(title);
+    setTime(time);
   }
 
   return (
     <View style={styles.container}>
-   <Image style={styles.image}
+      <Image
+        style={styles.image}
         source={{
-        uri: url,
+          uri: url,
         }}
-        opacity={0.6}
+        opacity={0.9}
       />
+      <SafeAreaView style={styles.textView}>
+        <Text style={styles.text}>NOW ON AIR</Text>
+        <Text style={styles.textTitle}>{title}</Text>
+        <Text style={styles.textTime}>{time}</Text>
+      </SafeAreaView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 4,
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor:'grey',
+    backgroundColor: 'grey',
     borderRadius: 10,
     borderColor: 'white',
-    borderWidth: 2,
-    borderStyle: 'solid'
+    borderWidth: 1,
+    borderStyle: 'solid',
   },
-  playBtn: {
-    padding: 20,
+ 
+  textView: {
+flex: 1,
+  flexDirection: 'column',
+    right: 0,
+    position: 'absolute',
+    bottom: 10,
+    left: 0,
+    textAlign: 'left',
+    marginLeft: 10,
+
   },
+  text: {
+    color: 'white',
+    fontSize: 10, 
+  },
+  textTitle: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textTransform: 'capitalize'  
+  },
+  textTime: {
+    color: 'white',
+    fontSize: 10,
+  },
+
+
   image: {
     width: 300,
     flex: 3,
     justifyContent: 'center',
-    margin:0,
-    borderWidth: 2,
-    borderColor: 'white',
+    margin: 0,
     borderRadius: 10,
-
+    borderColor: 'white',
+    borderWidth: 1,
+    borderStyle: 'solid',
   },
 });
-  
-
 
 export default Cover;
--- src/footer/index.js
+++ src/footer/index.js
@@ -14,6 +14,7 @@ const Footer = () => {
       <View style={styles.footerView}>
         <FontAwesomeIcon icon={["fab", "instagram"]} style={styles.icons} size={40} onPress={() => Linking.openURL('https://www.instagram.com/hotelradioparis/?hl=fr')} />
         <FontAwesomeIcon icon={["fab", "tiktok"]} style={styles.icons} size={40} onPress={() => Linking.openURL('https://www.tiktok.com/@hotelradioparis?')}/>
+        <FontAwesomeIcon icon={["fab", "soundcloud"]} style={styles.icons} size={40}  onPress={() => Linking.openURL('https://soundcloud.com/hotel-radio-paris')} />
         <FontAwesomeIcon icon={["fab", "mixcloud"]} style={styles.icons} size={40}  onPress={() => Linking.openURL('https://www.mixcloud.com/hotelradioparis/stream/')} />
         <FontAwesomeIcon icon={faShoppingCart} style={styles.icons} size={40}   onPress={() => Linking.openURL('https://hotelradioparis.bigcartel.com/')}  />
       </View>
